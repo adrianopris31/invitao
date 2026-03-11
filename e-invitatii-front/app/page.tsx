dae92d1e552566f4c7f3523a21b810f5597bc2f9
Adrian <115884887+ironOmega12@users.noreply.github.com>
2026-03-11 13:26:14 +0200

    Update page.tsx

--- e-invitatii-front/app/page.tsx
+++ e-invitatii-front/app/page.tsx
@@ -10,10 +10,7 @@ export default function Home() {
                     </h1>
                     <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
                         Verifica <a className="font-medium text-zinc-950 dark:text-zinc-50">link-ul</a> primit sau{" "}
-                        <a
-                            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                            className="font-medium text-zinc-950 dark:text-zinc-50"
-                        >
+                        <a href="" className="font-medium text-zinc-950 dark:text-zinc-50">
                             contacteaza-l
                         </a>{" "}
                         pe cel care ti l-a trimis.
